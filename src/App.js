56cd9cf11cc50b8c1eb06f0ac152115959456f5d
Diego Choli Conceição <48794883+diegocholi@users.noreply.github.com>
2021-08-29 22:59:08 -0300

    Update token API

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
-    authorization: 'Bearer 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',
+    authorization: 'Bearer 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',
   },
   cache: new InMemoryCache({
     typePolicies: {
